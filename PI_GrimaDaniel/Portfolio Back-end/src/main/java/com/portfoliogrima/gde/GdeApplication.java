a619c14da3abfcc707250b6137714f711503abd1
DamjoCAT <tdanit3238@gmail.com>
2023-02-23 20:41:47 -0300

    se crea carpeta back end

---
+++ PI_GrimaDaniel/Portfolio Back-end/src/main/java/com/portfoliogrima/gde/GdeApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliogrima.gde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdeApplication.class, args);
+	}
+
+}
---
+++ PI_GrimaDaniel/Portfolio Back-end/src/test/java/com/portfoliogrima/gde/GdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliogrima.gde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
