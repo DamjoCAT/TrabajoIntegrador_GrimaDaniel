0c69b59363a65ccb61b642b7b8854559164668c4
DamjoCAT <tdanit3238@gmail.com>
2023-02-23 19:24:36 -0300

    termine el front end

--- PI_GrimaDaniel/src/app/app.module.ts
+++ PI_GrimaDaniel/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardySoftComponent } from './components/hardy-soft/hardy-soft.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProyectoComponent } from './components/proyecto/proyecto.component';
     ExperienciaComponent,
     EducacionComponent,
     HardySoftComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
